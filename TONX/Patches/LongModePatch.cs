4868c4043c7b28d1580676ed3cc9d7f883a17bb9
Slok7565 <Slok7565@qq.com>
2025-02-02 22:06:27 +0800

    HorseMode => LongMode(2024Apr)

---
+++ TONX/Patches/LongModePatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace TONX;
+
+//来源：https://github.com/ykundesu/SuperNewRoles/blob/master/SuperNewRoles/Patches/LongModePatch.cs
+[HarmonyPatch(typeof(AprilFoolsMode), nameof(AprilFoolsMode.ShouldLongAround))]
+public static class LongModePatch
+{
+    public static bool Prefix(ref bool __result)
+    {
+        __result = Main.LongMode.Value;
+        return false;
+    }
+}
