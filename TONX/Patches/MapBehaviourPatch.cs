86c02eb21962e8d683f43cd0dc855f59f8589b06
Mousse <nidawosa1234@163.com>
2025-02-09 11:22:34 +0800

    Update MapBehaviour

---
+++ TONX/Patches/MapBehaviourPatch.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace TONX;
+
+[HarmonyPatch]
+public class MapBehaviourPatch
+{
+    private static Dictionary<PlayerControl, SpriteRenderer> herePoints = new Dictionary<PlayerControl, SpriteRenderer>();
+    private static bool ShouldShowRealTime => !PlayerControl.LocalPlayer.IsAlive() || PlayerControl.LocalPlayer.Is(Roles.Core.CustomRoles.GM) || Main.GodMode.Value;
+    [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.ShowNormalMap)), HarmonyPostfix]
+    public static void ShowNormalMapPostfix(MapBehaviour __instance)
+    {
+        if (!ShouldShowRealTime) return;
+        InitializeCustomHerePoints(__instance);
+    }
+    [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.ShowSabotageMap)), HarmonyPostfix]
+    public static void ShowSabotageMapPostfix(MapBehaviour __instance)
+    {
+        if (!ShouldShowRealTime) return;
+        InitializeCustomHerePoints(__instance);
+    }
+
+    public static void InitializeCustomHerePoints(MapBehaviour __instance)
+    {
+        __instance.DisableTrackerOverlays();
+        // 删除旧图标
+        foreach (var oldHerePoint in herePoints)
+        {
+            if (oldHerePoint.Value == null) continue;
+            Object.Destroy(oldHerePoint.Value.gameObject);
+        }
+        herePoints.Clear();
+
+        // 创建新图标
+        foreach (var pc in PlayerControl.AllPlayerControls)
+        {
+            if (!pc.AmOwner && pc != null)
+            {
+                var herePoint = Object.Instantiate(__instance.HerePoint, __instance.HerePoint.transform.parent);
+                herePoints.Add(pc, herePoint);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.FixedUpdate)), HarmonyPostfix]
+    public static void FixedUpdatePostfix(MapBehaviour __instance)
+    {
+        if (!ShouldShowRealTime) return;
+        foreach (var kvp in herePoints)
+        {
+            var pc = kvp.Key;
+            var herePoint = kvp.Value;
+            if (herePoint == null) continue;
+            herePoint.gameObject.SetActive(false);
+            if (pc == null || __instance.countOverlay.gameObject.active) continue;
+            herePoint.gameObject.SetActive(true);
+
+            // 设置图标颜色
+            herePoint.material.SetColor(PlayerMaterial.BodyColor, pc.Data.Color);
+            herePoint.material.SetColor(PlayerMaterial.BackColor, pc.Data.ShadowColor);
+            herePoint.material.SetColor(PlayerMaterial.VisorColor, Palette.VisorColor);
+
+            // 设置图标位置
+            var vector = pc.transform.position;
+            vector /= ShipStatus.Instance.MapScale;
+            vector.x *= Mathf.Sign(ShipStatus.Instance.transform.localScale.x);
+            vector.z = -1f;
+            herePoint.transform.localPosition = vector;
+        }
+    }
+
+    [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.Close)), HarmonyPostfix]
+    public static void ClosePostfix(MapBehaviour __instance)
+    {
+        if (!ShouldShowRealTime) return;
+        foreach (var kvp in herePoints)
+        {
+            var herePoint = kvp.Value;
+            if (herePoint == null) continue;
+            herePoint.gameObject.SetActive(false);
+        }
+    }
+}
