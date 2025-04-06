5a6a0264ee550a483fb9716084a66b4465df4e59
Mousse <nidawosa1234@163.com>
2025-04-06 13:11:34 +0800

    Small Fix

--- TONX/Patches/AprilFoolsModePatch.cs
+++ TONX/Patches/AprilFoolsModePatch.cs
@@ -5,6 +5,6 @@ namespace TONX;
 public static class AprilFoolsModePatch
 {
     public static bool FlipSkeld => AprilFoolsMode.ShouldFlipSkeld();
-    public static bool HorseMode => AprilFoolsMode.ShouldFlipSkeld();
-    public static bool LongMode => AprilFoolsMode.ShouldFlipSkeld();
+    public static bool HorseMode => AprilFoolsMode.ShouldHorseAround();
+    public static bool LongMode => AprilFoolsMode.ShouldLongAround();
 }
