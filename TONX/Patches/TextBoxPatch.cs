c2126cb2ee84a44ff7eee64aea42efa7307a44c1
Slok7565 <Slok7565@qq.com>
2025-08-17 18:55:43 +0800

    update

--- TONX/Patches/TextBoxPatch.cs
+++ TONX/Patches/TextBoxPatch.cs
@@ -19,11 +19,11 @@ public class TextBoxPatch
         { "“", " ''" },
         { "”", "'' " },
         { "！", "! " },
+        { "\r\n", " "},
     };
     [HarmonyPatch(nameof(TextBoxTMP.SetText)), HarmonyPrefix]
     public static bool ModifyCharacterLimit(TextBoxTMP __instance, [HarmonyArgument(0)] ref string input, [HarmonyArgument(1)] string inputCompo = "")
     {
-        input = input.Replace("\r\n", " ");
         __instance.characterLimit = AmongUsClient.Instance.AmHost ? 999 : 300;
         if (input.Length < 1) return true;
         string before = input[^1..];
