25b2eb8daf06dfa03c59fae98fe8553603ac7989
Slok7565 <Slok7565@qq.com>
2025-10-03 17:13:46 +0800

    都支持安卓了改个minor版本号不过分吧~

--- TONX/main.cs
+++ TONX/main.cs
@@ -34,7 +34,7 @@ public class Main : BasePlugin
     // == 版本相关设定 / Version Config ==
     public const string LowestSupportedVersion = "2025.9.9"; // 17.0.0
     public static readonly bool IsPublicAvailableOnThisVersion = false;
-    public const string PluginVersion = "3.1.1";
+    public const string PluginVersion = "3.2.0";
     public const int PluginCreation = 1;
     // == 链接相关设定 / Link Config ==
     public static readonly bool ShowWebsiteButton = false;
